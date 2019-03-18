b8a5fd3f9fbb213f5fe65dced36d894de3872634
akmal zamel <akmalzamel@gmail.com>
2019-03-18 14:30:19 +0800

    Fixed layout, modified landpage, added error alert

---
+++ resources/views/inc/messages.blade.php
@@ -0,0 +1,25 @@
+@if(count($errors) > 0)
+@foreach($errors->all() as $error)
+<div class="alert alert-danger" role="alert">
+    {{$error}}
+</div>
+@endforeach
+@endif
+
+@if(session('success'))
+<div class="alert alert-success" role="alert">
+    {{session('success')}}
+</div>
+@endif
+
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+    {{session('status')}}
+</div>
+@endif
+
+@if(session('error'))
+<div class="alert alert-danger" role="alert">
+    {{session('error')}}
+</div>
+@endif 
